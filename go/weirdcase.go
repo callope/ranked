576bac65e8d94e1e84fab4868e005098456022ed
callope <callope@proton.me>
2025-01-10 14:54:56 -0300

    forgot to implement this

--- go/weirdcase.go
+++ go/weirdcase.go
@@ -3,11 +3,19 @@ package kata
 import "strings"
 
 func toWeirdCase(s string) string {
-
   var r string
 
-  for i, c := range s {
+  input := strings.Split(s, " ")
+
+  for _, word := range input {
+    for i, l := range word {
+      if i % 2 == 0 {
+        r += strings.ToUpper(string(l))
+      } else { r += string(l) }
+    }
     
+    r += string(" ")
   }
 
+  return r
 }
